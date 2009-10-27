8fd556aa92e0b20d4083a07b1aa9bd064f6c9fe0
Minastirith <anthony.lacagnina@gmail.com>
2009-10-27 01:58:13 +0100

    Ajout rep scripts

---
+++ scripts/install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#########################################################
+#   Script d'automatisation d'installation des paquets  #
+#   Parce que se taper tout à la main non merci ...     #
+#########################################################
+
+# Installation des paquets avec Pacman
+sudo pacman -S pidgin pidgin-encryption pidgin-facebookchat vlc mpd sonata wine firefox conky lastfm-client zsh oss flashplugin texlive-latex3 texlive-latexextra wine taglib tagpy libnotify pidgin-libnotify python-notify zsi unzip unrar emesene deluge thunar xcompmgr
+
+# Insertion du chemin pour Yaourt dans /etc/pacman.conf
+sudo echo -e "\n" >> /etc/pacman.conf
+sudo echo "[Archlinuxfr]" >> /etc/pacman.conf
+sudo echo "Server = http://repo.archlinux.fr/i686" >> /etc/pacman.conf
+
+# Mise à jour du cache et update des paquets
+sudo pacman -Syu
+
+# Installation de Yaourt
+sudo pacman -S yaourt
+
+# Remise à jour du cache et update des paquets
+sudo pacman -Syu
+
+# Installation des paquets restants depuis yaourt
+yaourt -S virtualbox_bin virtualbox-additions chromium-snapshot
