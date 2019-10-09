7abb4bf4a38022cd50419626df262f4d3bb8df78
Minastirith <anthony.lacagnina@gmail.com>
2019-10-09 10:34:56 +0200

    Add files via upload

---
+++ key_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Please enter path to scan"
+read SCAN_PATH
+
+find ${SCAN_PATH} -name "authorized_keys" -or -name "*.pub" -type f > key_list 2>/dev/mull
+
+
+while read line
+do
+
+ssh-keygen -l -f $line | awk '{ if ($4 == "(DSA)" || $4 == "(RSA)" && $1 <= 2048) print $3,": key is weak",$4,"and",$1,"bits"; }'
+
+done < key_list
